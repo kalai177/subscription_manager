18709003c8d228b7fb8c9699dbbeb241e5cff636
kalai <kalaiyarasi.ec19@bitsathy.ac.in>
2022-02-16 15:52:34 +0530

    subscription

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose'
-const schema=new mongoose.Schema({
-    username:{type:String, required:true, unique:true},
-    mailid:{type:String,required:true,unique:true},
-    password:{type:String},
-    subscription_detail:{
-        plan_type:{type:String},
-        amount:{type:String},
-        valid_till:{type:String}
-    },
-    expires_at:{type:String}
-})
+import mongoose from "mongoose";
+const schema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  mailid: { type: String, required: true, unique: true },
+  password: { type: String },
+  subscription_detail: {
+    plan_type: { type: String },
+    amount: { type: String },
+    valid_for: { type: String },
+  },
+  expires_at: { type: String },
+});
 
-const User=mongoose.model("users",schema)
-export default User
+const User = mongoose.model("users", schema);
+export default User;
